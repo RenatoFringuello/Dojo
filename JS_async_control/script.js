2ba735b9e4826a3d58bdf0defb89a47d58b7a44f
RenatoFringuello <renatofringuello@hotmail.it>
2023-04-05 01:00:25 +0200

    add some style and create dom injection for posts

--- JS_async_control/script.js
+++ JS_async_control/script.js
@@ -1,25 +1,50 @@
-let id = Math.floor(Math.random() * 10) + 1;
+const ulDom = document.getElementById('posts');
 const baseUri = 'https://jsonplaceholder.typicode.com';
 let post, user;
+let posts = [];
+let nPosts = 10;
 
-async function getUserData() {
+function injectPosts(posts){
+    posts.forEach(post => {
+        const liDom = document.createElement('li');
+        liDom.className = 'mb-3';
+        liDom.innerHTML = `
+        <div class="card">
+            <h1 class="title mb-3">${post.post.title}</h1>
+            <div>
+                <h3 class="author mb-3">${post.user.name}</h3>
+                <p class="content">${post.post.body}</p>
+            </div>
+        </div>
+        `;
+        ulDom.appendChild(liDom);
+    });
+}
+
+async function getUserData(post) {
 
     const userCall = await fetch(baseUri + `/users/${post.id}`)
     let userResponse = userCall.json();
     userResponse.then((response)=>{
                 user = response;
+                posts.push({
+                    post : post,
+                    user : user
+                })
             })
             .catch((err)=>{
                 console.error(err);
             })
             .then(()=>{
-                console.log('user',user);
+                if(posts.length == nPosts){
+                    injectPosts(posts)
+                }
             });
 }
 
-async function getPostData(callback) {
+async function getPostData(postId, callback) {
 
-    const postCall = await fetch(baseUri + `/posts/${id}`)
+    const postCall = await fetch(baseUri + `/posts/${postId}`)
     let postResponse = postCall.json();
     postResponse.then((response)=>{
                 post = response;
@@ -28,7 +53,10 @@ async function getPostData(callback) {
                 console.error(err);
             })
             .then(()=>{
-                console.log('post',post);
-                callback()
+                callback(post)
             });
+}
+
+for (let i = 0; i < nPosts; i++) {
+    getPostData(i+1, getUserData);
 }
