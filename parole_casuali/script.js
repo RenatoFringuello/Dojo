24b60e97196e71eb99a21ae1348e13955c74eedb
RenatoFringuello <renatofringuello@hotmail.it>
2023-04-04 22:08:36 +0200

    implement sentence

--- parole_casuali/script.js
+++ parole_casuali/script.js
@@ -1,19 +1,29 @@
+const sentenceDom = document.getElementById('sentenceDom');
 let words = [];
 const nWords = 20;
 const apiUrl = 'https://flynn.boolean.careers/exercises/api/random/word';
 
+function injectSentence(words, domElement){
+    domElement.innerText = '';
+    for (let i = 0; i < words.length; i++) {
+        domElement.innerText += words[i];
+    }
+    domElement.innerText = domElement.innerText.split('.').join(' ');
+}
+
 function getWord(){
     axios.get(apiUrl)
     .then((response)=>{
-        words.push(response.data.response);
+        words.push(response.data.response + '.');
     })
     .catch((err)=>{
         console.error(err);
     })
     .then(()=>{
         //display the list of words
-        console.clear();
+        // console.clear();
         console.log(words);
+        injectSentence(words, sentenceDom);
     });
 }
 
