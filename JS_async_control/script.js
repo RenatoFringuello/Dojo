3c22c32544b3e85aa335bd82d73573af7984a88d
RenatoFringuello <renatofringuello@hotmail.it>
2023-04-04 22:57:39 +0200

    get call for post and user

---
+++ JS_async_control/script.js
@@ -0,0 +1,36 @@
+let id = Math.floor(Math.random() * 10);
+const baseUri = 'https://jsonplaceholder.typicode.com';
+let post, user;
+
+async function getuserData() {
+
+    const userCall = await fetch(baseUri + `/users/${post.id}`)
+    let userResponse = userCall.json();
+    userResponse.then((response)=>{
+                user = response;
+            })
+            .catch((err)=>{
+                console.error(err);
+            })
+            .then(()=>{
+                console.log('user',user);
+            });
+}
+
+async function getpostData() {
+
+    const postCall = await fetch(baseUri + `/posts/${id}`)
+    let postResponse = postCall.json();
+    postResponse.then((response)=>{
+                post = response;
+            })
+            .catch((err)=>{
+                console.error(err);
+            })
+            .then(()=>{
+                console.log('post',post);
+                getuserData()
+            });
+}
+
+getpostData();
