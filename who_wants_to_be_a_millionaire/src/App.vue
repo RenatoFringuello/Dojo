cf4de9752eff561ce2be2ec4abd8d2b812be69bc
RenatoFringuello <renatofringuello@hotmail.it>
2023-04-05 12:39:21 +0200

    add app logo in app

--- who_wants_to_be_a_millionaire/src/App.vue
+++ who_wants_to_be_a_millionaire/src/App.vue
@@ -1,15 +1,28 @@
 <script>
+import AppQuestion from './components/AppQuestion.vue';
+import AppLogo from './components/AppLogo.vue';
 export default {
   name: 'App',
   components: {
+    AppQuestion,
+    AppLogo,
   }
 }
 </script>
 
 <template>
-
+  <main class="position-relative">
+    <AppLogo />
+    <AppQuestion />
+  </main>
 </template>
 
 <style lang="scss">
   @use './style/general.scss' as *;
+
+  main{
+    background:#040444;
+    min-height: 100vh;
+  }
+
 </style>
---
+++ who_wants_to_be_a_millionaire/src/components/AppLogo.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    name:'AppLogo'
+}
+</script>
+
+<template>
+    <img src="../assets/sparkles.png" alt="logo" class="position-absolute sparkles">
+    <img src="../assets/sparkles.png" alt="logo" class="position-absolute sparkles reverse">
+    <img src="../assets/sparkles.png" alt="logo" class="position-absolute sparkles">
+    <img src="../assets/sparkles.png" alt="logo" class="position-absolute sparkles reverse">
+    <img src="../assets/mil_logo.avif" alt="logo" class="position-absolute w-50">
+</template>
+
+<style lang="scss">
+    img{
+      object-fit: contain;
+      object-position: center;
+      transform: translateX(-50%);
+      left:50%;
+      filter: drop-shadow(0px 0px 50px #fff8);
+      -webkit-filter: drop-shadow(0px 0px 50px #fff8);
+
+      &.sparkles{
+        filter: brightness(0) invert(1);
+        width: 35%;
+        transform: translate(-50%, 30%) rotate(120deg);
+        left:calc(50% - 70px);
+        
+        &.reverse{
+          transform: translate(-50%, 60%) rotate(-60deg);
+          left:calc(50% + 70px);
+        }
+      }
+    }
+</style>
