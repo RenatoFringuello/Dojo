8495b85a908a9c69b6af61bf88fc31a9c28f250d
RenatoFringuello <renatofringuello@hotmail.it>
2023-04-07 11:56:11 +0200

    fix app logo row bug

--- who_wants_to_be_a_millionaire/src/components/AppLogo.vue
+++ who_wants_to_be_a_millionaire/src/components/AppLogo.vue
@@ -5,14 +5,14 @@ export default {
 </script>
 
 <template>
-  <div class="row">
-    <div class="col-12 col-md-6 position-relative m-auto">
-      <div class="wrapper position-absolute w-100 h-100">
+  <div class="d-flex flex-column">
+    <div class="col-12 col-sm-8 col-md-6 m-auto">
+      <div class="wrapper position-relative">
         <img src="../assets/sparkles.png" alt="logo" class="position-absolute w-50 sparkles">
         <img src="../assets/sparkles.png" alt="logo" class="position-absolute w-50 sparkles reverse">
         <img src="../assets/sparkles.png" alt="logo" class="position-absolute w-50 sparkles">
         <img src="../assets/sparkles.png" alt="logo" class="position-absolute w-50 sparkles reverse">
-        <img src="../assets/mil_logo.png" alt="logo" class="position-absolute w-50">
+        <img src="../assets/mil_logo.png" alt="logo" class="d-block mx-auto w-50">
       </div>
     </div>
   </div>
@@ -20,12 +20,10 @@ export default {
 
 <style lang="scss" scoped>
 .wrapper{
-  padding-top: 5rem;
+  padding-top: 3rem;
   img{
     object-fit: contain;
     object-position: center;
-    transform: translateX(-50%);
-    left:50%;
     filter: drop-shadow(0px 0px 50px #fff8);
     -webkit-filter: drop-shadow(0px 0px 50px #fff8);
 
